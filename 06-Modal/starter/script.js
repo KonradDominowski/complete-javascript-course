673e3bcc525b153ac791d33e220efb7f25423731
Konrad Dominowski <zehkan@gmail.com>
2022-12-23 14:55:38 +0100

    coding challenge project 2 - Modal Window

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,29 @@
 'use strict';
+const modalBtns = document.querySelectorAll('.show-modal')
+const modal = document.querySelector('.modal')
+const closeModalBtn = document.querySelector('.close-modal')
+const openModalBtn = document.querySelector('show-modal')
+const overlay = document.querySelector('.overlay')
+
+
+const closeModal = () => {
+	modal.classList.add('hidden');
+	overlay.classList.add('hidden');
+}
+
+const openModal = () => {
+	modal.classList.remove('hidden')
+	overlay.classList.remove('hidden')
+}
+
+for (const btn of modalBtns) btn.addEventListener('click', openModal)
+
+closeModalBtn.addEventListener('click', closeModal)
+overlay.addEventListener('click', closeModal)
+
+document.addEventListener('keydown', event => {
+	if (event.key === 'Escape' && !modal.classList.contains('hidden')) {
+		closeModal()
+	}
+}
+)
